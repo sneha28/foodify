3ebdc520ea3d0ef723114d3a5b311c6f697a4dd3
sneha28 <snehad925@gmail.com>
2024-08-31 22:07:21 +0530

    Update StoreContext.jsx
    updated the backend url

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://foodify-backend-k86p.onrender.com"
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState([])
 
